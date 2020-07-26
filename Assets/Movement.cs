5159a06b6111ae5fe2ff8799026e597b83ff6fcd
Lachlan <mindcraftlachlan@outlook.com>
2020-07-26 22:43:16 +1000

    A* pathfinding and player scale with 'y' coord

---
+++ Assets/Movement.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+
+    
+    public float Size = 1;
+    public float sizeScale = 0.5f;
+
+    SpriteRenderer sp;
+
+
+    public GameObject targetObject;
+    Vector3 target;
+
+    RaycastHit2D hit;
+    // Start is called before the first frame update
+    void Start()
+    {
+        sp = GetComponent<SpriteRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        sp.transform.localScale = Vector3.one * Size * (1 - (transform.position.y/sizeScale));
+
+
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            hit = Physics2D.Raycast(Camera.main.ScreenToWorldPoint(Input.mousePosition), Vector3.zero);
+            target = hit.point;
+        }
+
+        //if (hit.collider != null)
+        //{
+            targetObject.transform.position = target;
+        //}
+    }
+}
