ec2e75ef78a1c3dda862d230ede713788525ddee
Kiarny Petersen <pokemonindigo991@gmail.com>
2020-07-27 11:20:25 +1000

    Added my testing scene

---
+++ Assets/Scripts/Movement.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+	public float speed = 1f;
+
+	private Rigidbody rig;
+
+
+	// Start is called before the first frame update
+	void Start()
+    {
+		rig = GetComponent<Rigidbody>();
+	}
+
+    // Update is called once per frame
+    void Update()
+    {
+		float hAxis = Input.GetAxis("Horizontal");
+		Vector3 movement = new Vector3(hAxis, 0, 0) * speed * Time.deltaTime;
+		rig.MovePosition(transform.position + movement);
+	}
+}
