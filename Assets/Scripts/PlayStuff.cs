293405126114cf6063cc23dc0bd53bc51c13c26f
Lachlan <mindcraftlachlan@outlook.com>
2020-08-17 19:13:09 +1000

    more main scene stuff

---
+++ Assets/Scripts/PlayStuff.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayStuff : MonoBehaviour
+{
+    AudioSource aSource;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        aSource = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void PlayAudioSound(AudioClip sound)
+    {
+        aSource.PlayOneShot(sound);
+    }
+}
--- Assets/Scripts/RayCastOnClick.cs
+++ Assets/Scripts/RayCastOnClick.cs
@@ -9,8 +9,12 @@ public class RayCastOnClick : MonoBehaviour
     GameObject HitObject;
 
     public GameObject PlayerWalkTarget;
-    
+
+    public bool HasFlame = false;
+    GameObject heldFlame;
+    public GameObject player;
    
+
         
     public enum ObjectTag
     {
@@ -18,7 +22,8 @@ public class RayCastOnClick : MonoBehaviour
         Walkable, 
         PullTab, 
         Exit, 
-        Candle
+        Candle,
+        Flame
     }
 
     // Start is called before the first frame update
@@ -46,21 +51,47 @@ public class RayCastOnClick : MonoBehaviour
             //make sure to leave the very last "else" statement so that we can catch anything thats not in yet
             if(HitObject.tag == ObjectTag.PullTab.ToString()) // pull tab
             {
+                Toggleable tog = HitObject.GetComponent<Toggleable>();
+                if( tog != null)
+                {
+                    if (tog.IsActive)
+                    {
+                        Animator anim = HitObject.GetComponent<Animator>();
+                        anim.SetBool("IsTriggered", !anim.GetBool("IsTriggered"));
+                    }
+                }
+                else
+                {
+                    Animator anim = HitObject.GetComponent<Animator>();
+                    anim.SetBool("IsTriggered", !anim.GetBool("IsTriggered"));
+                }
+
                 
-                Animator anim = HitObject.GetComponent<Animator>();
-                anim.SetBool("IsTriggered", !anim.GetBool("IsTriggered"));
             }
             else if (HitObject.tag == ObjectTag.Walkable.ToString() || HitObject.tag == ObjectTag.Untagged.ToString())
             {
                 PlayerWalkTarget.transform.position = HitPosition;
             }
-            else if(HitObject.tag == ObjectTag.Exit.ToString())
-            {
-
-            }
             else if(HitObject.tag == ObjectTag.Candle.ToString())
             {
+                if (HasFlame && heldFlame != null)
+                {
+                    heldFlame.transform.position = HitObject.transform.position;
+                    heldFlame.transform.parent = HitObject.transform;
 
+                    heldFlame.GetComponent<candle>().SetTabActive();
+                }
+            }
+            else if(HitObject.tag == ObjectTag.Flame.ToString())
+            {
+                if (!HasFlame)
+                {
+                    heldFlame = HitObject;
+                    heldFlame.transform.position = player.transform.position;
+                    heldFlame.transform.parent = player.transform;
+
+                    heldFlame.GetComponent<candle>().SetTabInactive();
+                }
             }
             else
             {
