3c95437fb34efdc955f4f2d4a0af4fbf9bc0e759
Kiarny Petersen <pokemonindigo991@gmail.com>
2020-10-06 18:55:48 +1100

    World Space UI
    Testing in my scene with UI in worldspace

---
+++ Assets/Scripts/TestingUI.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestingUI : MonoBehaviour
+{
+	public Animator UI;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+	public void StartAnimation()
+	{
+		UI.SetBool("startPressed", true);
+	}
+
+	public void OptionsPressed()
+	{
+		print("Still working, sadly");
+	}
+}
