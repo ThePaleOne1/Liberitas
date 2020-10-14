58f8da81a30aafd604eb46831511245f4e36d1f0
Lachlan <mindcraftlachlan@outlook.com>
2020-10-14 17:45:56 +1100

    fixed level 1 assets

---
+++ Assets/Scripts/TransitionTesting.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TransitionTesting : MonoBehaviour
+{
+    public Camera camera;
+    public Material material;
+    public RenderTexture rendTex;
+
+    public int resHeight;
+    public int resWidth;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+
+        rendTex.width = camera.pixelWidth;
+        rendTex.height = camera.pixelHeight;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            material.mainTexture = rendTex;
+        }
+    }
+}
