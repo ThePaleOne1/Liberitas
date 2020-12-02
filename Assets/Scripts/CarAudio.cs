c4bd74b6f9a68726a6405a53d3cf6433b50bcfcd
Kiarny Petersen <pokemonindigo991@gmail.com>
2020-12-03 03:32:49 +1100

    Car and Lantern audio

--- Assets/Scripts/CarAudio.cs
+++ Assets/Scripts/CarAudio.cs
@@ -4,15 +4,50 @@ using UnityEngine;
 
 public class CarAudio : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+	//public AudioSource aSource;
+	public AudioSource startSource;
+	public AudioSource movingSource;
+	public AudioSource stopSource;
+	//public AudioClip carsMove;
+
+	public bool carsStart = true;
+	//public bool carsEnd = false;
+
+	public LevelChange lvlChange;
+
+	// Start is called before the first frame update
+	void Start()
     {
-        
+		startSource.loop = true;
+		stopSource.loop = true;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
-    }
+		if (lvlChange.CurrentLevel == 3 && carsStart == true)
+		{
+			startSource.Play();
+			carsStart = false;
+		}
+		if (lvlChange.CurrentLevel != 3)
+		{
+			startSource.Stop();
+			stopSource.Stop();
+			carsStart = true;
+		}
+	}
+
+	public void CarsMoving()
+	{
+		movingSource.Play();
+		startSource.Stop();
+
+	}
+
+	public void CarsStop()
+	{
+		movingSource.Stop();
+		stopSource.Play();
+	}
 }
---
+++ Assets/Scripts/LanternAudio.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LanternAudio : MonoBehaviour
+{
+	public AudioSource aSource;
+	public AudioClip lanternOn;
+	public AudioClip lanternOff;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+	public void PlayAudioOn()
+	{
+		aSource.PlayOneShot(lanternOn);
+	}
+
+	public void PlayAudioOff()
+	{
+		aSource.PlayOneShot(lanternOff);
+	}
+}
